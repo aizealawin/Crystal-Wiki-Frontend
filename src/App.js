cf9b83244c1803519f746d6487c24faae8bebbe2
aizealawin <ktmudalige@gmail.com>
2022-12-13 09:49:40 -0500

    developed nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import logo from './logo.svg'
+import { Route, Routes } from 'react-router-dom'
+import Nav from './Components/Nav'
+import Home from './Pages/Home'
+import Login from './Pages/Login'
+import Register from './Pages/Register'
+import Articles from './Components/Articles'
+
 import './Styles/App.css'
 
 function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div className="App">
+      <header className="App-Header">
+        <Nav />
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/articles" element={<Articles />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <nav>
+      <Link to='/'>Home</Link>
+      <Link to='/login'>Login</Link>
+      <Link to='/articles'>Articles</Link>
+    </nav>
+  )
+}
+
+export default Nav
