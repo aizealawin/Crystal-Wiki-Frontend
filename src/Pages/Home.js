9583c874aba231161896cd72a068521c51695ccd
aizealawin <ktmudalige@gmail.com>
2022-12-14 20:37:16 -0500

    front page finished

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,10 +5,31 @@ const Home = () => {
   return (
     <div className="home">
       <div className="intro-holder">
-        <div>
-          <div className="welcome">Welcome To</div>
-          <div className="kagrea-name">Kagrea</div>
-          <h4>A World of Magic - Crystal: 1</h4>
+        <div className="container">
+          <div className="home-body">
+            <div className="welcome">Welcome To</div>
+            <div className="kagrea-name">Kagrea</div>
+            <h4>A World of Magic - Crystal: 1</h4>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+            </p>
+            {/* leads to login / register */}
+            <button className="join-button">Join us!</button>
+          </div>
+          <div>
+            <img
+              className="kagrea-crystal"
+              src="https://i.imgur.com/r5dSDaO.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="about-us">
+        <div className="about-us-container">
+          <h3>About us</h3>
+          <img className="starline" src="https://i.imgur.com/Ab5dZYz.png" />
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
@@ -18,29 +39,8 @@ const Home = () => {
             pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
             culpa qui officia deserunt mollit anim id est laborum.
           </p>
-          {/* leads to login / register */}
-          <button>Join us!</button>
+          <button className="history">Learn the History</button>
         </div>
-        <div>
-          <img
-            className="kagrea-crystal"
-            src="https://i.imgur.com/r5dSDaO.png"
-          />
-        </div>
-      </div>
-      <div className="about-us">
-        <h3>About us</h3>
-        <img className="starline" src="https://i.imgur.com/Ab5dZYz.png" />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </p>
-        <button>Learn the History</button>
       </div>
     </div>
   )
